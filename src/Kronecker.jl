eead3c6791ce6cef3c5e469047e4ed2d1e9930a3
Michiel Stock <michielfmstock@gmail.com>
2019-07-23 16:24:57 -0400

    Update Kronecker.jl

--- src/Kronecker.jl
+++ src/Kronecker.jl
@@ -4,7 +4,7 @@ module Kronecker
 export GeneralizedKroneckerProduct, AbstractKroneckerProduct, SquareKroneckerProduct, EigenKroneckerProduct, ShiftedKroneckerProduct
 export issquare, getmatrices, size, getindices, order, issymmetric
 export ⊗, kronecker, Matrix
-export tr, det, collect, inv, *, mult!, eigen, \, /, adjoint, transpose, conj,solve
+export tr, det, collect, inv, *, mult!, eigen, \, /, adjoint, transpose, conj, solve
 export getindex
 
 export genvectrick, genvectrick!

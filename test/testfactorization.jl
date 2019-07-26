45b7d4c165f4b09afe2b56e4989bf6e2c2f80e24
michielstock <michielfmstock@gmail.com>
2019-07-26 19:13:58 -0400

    :white_check_mark: testing `choleskey`

---
+++ test/testfactorization.jl
@@ -0,0 +1,22 @@
+using LinearAlgebra: cholesky
+
+A = [1 0 0.5;
+     0 2 0;
+     0.5 0 3]
+
+B = rand(4)
+B *= B'
+
+K = A ⊗ B
+
+@test isposdef(K)
+
+KC = cholesky(K)
+@test isposdef(K)
+@test size(K) == (12, 12)
+
+C = cholesky(collect(K))
+
+@test det(KC) ≈ det(C)
+@test logdet(KC) ≈ logdet(C)
+@test collect(inv(KC)) ≈ inv(C)

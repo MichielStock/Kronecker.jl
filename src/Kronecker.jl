237c79c35e61c03a5c98f7917ace5b9814461b6d
Michiel Stock <michielfmstock@gmail.com>
2019-07-30 20:03:09 +0200

    Update Kronecker.jl
    Removing exporting Kronecker types

--- src/Kronecker.jl
+++ src/Kronecker.jl
@@ -1,8 +1,5 @@
 module Kronecker
 
-export GeneralizedKroneckerProduct, AbstractKroneckerProduct, AbstractSquareKronecker
-export AbstractKroneckerSum, KroneckerSum
-export EigenKroneckerProduct, ShiftedKroneckerProduct
 export issquare, getmatrices, size, getindices, order, issymmetric, isposdef
 export ⊗, kronecker, Matrix, ⊕, kroneckersum
 export tr, det, logdet, collect, inv, +, *, mult!, eigen, /, adjoint, transpose, conj, solve, exp, kron

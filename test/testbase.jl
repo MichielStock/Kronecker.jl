f935bc2719a945160d69a103a6f76549aaca52d1
michielstock <michielfmstock@gmail.com>
2019-08-02 17:18:05 +0200

    :zap: mul! for reshaped

--- test/testbase.jl
+++ test/testbase.jl
@@ -56,6 +56,10 @@
 
     @testset "Vec trick" begin
         @test K * v ≈ X * v
+        V = rand(4, 3)
+        @test K * vec(V) ≈ collect(K) * vec(V)
+        @test_throws DimensionMismatch K * V
+        @test_throws DimensionMismatch K * reshape(V, 2, 6)
         K3 = A ⊗ B ⊗ C
         v3 = randn(size(K3, 2))
         @test K3 * v3 ≈ collect(K3) * v3

5486561223441b2a28f935c7952e53f156c1d28e
michielstock <michielfmstock@gmail.com>
2019-08-14 15:55:20 +0200

    :wrench: indexing KroneckerPower

--- src/Kronecker.jl
+++ src/Kronecker.jl
@@ -2,6 +2,7 @@ module Kronecker
 
 export GeneralizedKroneckerProduct, AbstractKroneckerProduct
 export AbstractKroneckerSum, KroneckerSum
+export KroneckerPower
 export EigenKroneckerProduct
 export issquare, order, issymmetric, isposdef, getmatrices
 export ⊗, kronecker, ⊕, kroneckersum
@@ -15,6 +16,7 @@ import Base: collect
 using SparseArrays: sparse
 
 include("base.jl")
+include("kroneckerpowers.jl")
 include("vectrick.jl")
 include("indexedkroncker.jl")
 include("eigen.jl")
--- src/kroneckerpowers.jl
+++ src/kroneckerpowers.jl
@@ -17,10 +17,14 @@ struct KroneckerPower{TA<:AbstractMatrix, N} <: AbstractKroneckerProduct
    A::TA
    pow::Integer
    function KroneckerPower(A::AbstractMatrix{T}, pow::Integer) where {T}
-        return new{typeof(A), pow}(A, pow)
+      @assert pow ≥ 2 "KroneckerPower only makes sense for powers greater than 1"
+      return new{typeof(A), pow}(A, pow)
     end
 end
 
-#TODO getindex
-#TODO size
-#TODO overloading all function
+getmatrices(K::KroneckerPower{T, N}) where {T, N} = (K.A, KroneckerPower(K.A, K.pow-1))
+getmatrices(K::KroneckerPower{T, 2}) where {T} = (K.A, K.A)
+getmatrices(K::KroneckerPower{T, 1}) where {T} = (K.A, )
+
+order(K::KroneckerPower) = K.pow
+Base.size(K::KroneckerPower) = size(K.A).^K.pow

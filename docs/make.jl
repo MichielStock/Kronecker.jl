4546658e8be138805d4ca0d124189fb123f29409
michielstock <michielfmstock@gmail.com>
2019-09-18 17:24:36 +0200

    :wrench: link to github pages

--- docs/make.jl
+++ docs/make.jl
@@ -3,3 +3,7 @@ using Documenter, Kronecker, LinearAlgebra
 makedocs(sitename="Kronecker.jl",
         authors = "Michiel Stock",
         modules = [Kronecker])
+
+deploydocs(
+        repo = "github.com/MichielStock/Kronecker.jl.git",
+        )

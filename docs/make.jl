5ef76b11da7cc11debe7fc55d4e998a6d823a9cd
michielstock <michielfmstock@gmail.com>
2019-09-19 10:03:55 +0200

    :wrench: add install for packages

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,13 @@
+using Pkg
+
+tmp_packages = ["Kronecker", "Documenter"]
+
+push!(LOAD_PATH,"../src/")
+
+Pkg.activate(".")
+
+Pkg.add.(tmp_packages) # IMPORTANT
+
 using Documenter, Kronecker, LinearAlgebra
 
 makedocs(sitename="Kronecker.jl",

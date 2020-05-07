2bb363b7b5dc773037cad41f13cacd032d4574b9
Elías Snorrason <eliassno@gmail.com>
2020-05-07 21:22:11 +0000

    Fixed deprecated 'names' to 'dimnames'

--- test/testnames.jl
+++ test/testnames.jl
@@ -5,15 +5,15 @@ using NamedDims
     ndA = NamedDimsArray(A, (:ia, :ja))
     ndB = NamedDimsArray(B, (:ib, :jb))
 
-    @test NamedDims.names(kronecker(ndA, ndB)) == (Symbol("iaᵡib"), Symbol("jaᵡjb"))
-    @test NamedDims.names(kronecker(ndA, B)) == (Symbol("iaᵡ_"), Symbol("jaᵡ_"))
-    @test NamedDims.names(kronecker(A, ndB)) == (Symbol("_ᵡib"), Symbol("_ᵡjb"))
+    @test NamedDims.dimnames(kronecker(ndA, ndB)) == (Symbol("iaᵡib"), Symbol("jaᵡjb"))
+    @test NamedDims.dimnames(kronecker(ndA, B)) == (Symbol("iaᵡ_"), Symbol("jaᵡ_"))
+    @test NamedDims.dimnames(kronecker(A, ndB)) == (Symbol("_ᵡib"), Symbol("_ᵡjb"))
 
     sum(abs2, kronecker(ndA, ndB) .- kron(A, B)) == 0
 
     @test 0 == @allocated Kronecker._join(:i, :j)
     @test 0 == @allocated Kronecker.kron_names((:ia, :ja), (:ib, :jb))
 
-    @test NamedDims.names(kronecker(ndA, 3)) == (Symbol("iaᵡiaᵡiaᵡia"), Symbol("jaᵡjaᵡjaᵡja"))
+    @test NamedDims.dimnames(kronecker(ndA, 3)) == (Symbol("iaᵡiaᵡiaᵡia"), Symbol("jaᵡjaᵡjaᵡja"))
     @test 0 == @allocated Kronecker.kron_names((:i, :j), Val(3))
 end

cd19ca6dae8fbe674b41f591e91e34f05e73f86a
Daniel Karrasch <Daniel.Karrasch@gmx.de>
2020-09-11 11:48:24 +0200

    update docs and Project.toml

--- docs/make.jl
+++ docs/make.jl
@@ -12,11 +12,22 @@ Pkg.add.(tmp_packages) # IMPORTANT
 
 using Documenter, Kronecker, LinearAlgebra
 
-makedocs(sitename="Kronecker.jl",
-        authors = "Michiel Stock",
-        format = :html,
-        modules = [Kronecker])
+makedocs(
+    sitename = "Kronecker.jl",
+    authors = "Michiel Stock",
+    format = Documenter.HTML(),
+    modules = [Kronecker],
+    pages = Any[
+        "Basic use" => "man/basic.md",
+        "Types" => "man/types.md",
+        "Multiplication" => "man/multiplication.md",
+        "Factorization methods" => "man/factorization.md",
+        "Indexed Kronecker products" => "man/indexed.md",
+        "Kronecker sums" => "man/kroneckersums.md",
+        "Kronecker powers and graphs" => "man/kroneckerpowers.md"
+    ]
+)
 
 deploydocs(
-        repo = "github.com/MichielStock/Kronecker.jl.git",
-        )
+    repo = "github.com/MichielStock/Kronecker.jl.git",
+)

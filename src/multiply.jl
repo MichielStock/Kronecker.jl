97642b0ae1c75ce06fb32e46d620eb20079431bd
Ejaaz Merali <emerali@users.noreply.github.com>
2020-09-12 18:54:17 -0400

    ldiv ops

--- src/multiply.jl
+++ src/multiply.jl
@@ -2,8 +2,12 @@
 @inline _alloc_temp_array(size_1::Int, x::AbstractMatrix{T}) where T = zeros(T, size_1, size(x, 2))
 
 for N in (1, 2)
-    for (op, kernel_name!) in [(:mul! => :_kron_mv_kernel_square!), (:ldiv! => :_kron_dv_kernel_square!)]
-        @eval function $kernel_name!(temp::AbstractArray{T, $N}, q::AbstractArray{T, $N}, n::Int, i_left::Int, m::AbstractMatrix{T}, i_right::Int) where T
+    for op in [:mul, :ldiv]
+        square_kernel! = Symbol("_kron_", op, "_kernel_square!")
+        rect_kernel! = Symbol("_kron_", op, "_kernel_rect!")
+
+        op! = Symbol(op, "!")
+        @eval function $square_kernel!(temp::AbstractArray{T, $N}, q::AbstractArray{T, $N}, n::Int, i_left::Int, m::AbstractMatrix{T}, i_right::Int) where T
             # apply kron(I(l), m, I(r)) where m is square to the given vector x, overwriting x in the process
 
             if m != I # if matrix is the identity, skip matmul/div
@@ -14,9 +18,9 @@ for N in (1, 2)
                     for i_r in 1:i_right
                         slc = base_i + i_r : i_right : top_i + i_r
                         if $N == 1
-                            @views q[slc] = $op(temp[1:n], m, q[slc])
+                            @views q[slc] = $op!(temp[1:n], m, q[slc])
                         else
-                            @views q[slc, :] = $op(temp[1:n, :], m, q[slc, :])
+                            @views q[slc, :] = $op!(temp[1:n, :], m, q[slc, :])
                         end
                     end
 
@@ -26,10 +30,8 @@ for N in (1, 2)
             end
             return q
         end
-    end
 
-    for (op, kernel_name) in [(:mul! => :_kron_mv_kernel_rect), (:ldiv! => :_kron_dv_kernel_rect)]
-        @eval function $kernel_name(temp::AbstractArray{T, $N}, q::AbstractArray{T, $N}, r_h::Int, c_h::Int, i_left::Int, m::AbstractMatrix{T}, i_right::Int) where T
+        @eval function $rect_kernel!(temp::AbstractArray{T, $N}, q::AbstractArray{T, $N}, r_h::Int, c_h::Int, i_left::Int, m::AbstractMatrix{T}, i_right::Int) where T
             # apply kron(I(i_left), m, I(i_right)) to the given vector q
 
             # don't bother checking for identity, since we know the matrix
@@ -47,9 +49,9 @@ for N in (1, 2)
                     slc_in  = base_i + i_r : i_right : i_r + top_i
                     slc_out = base_j + i_r : i_right : i_r + top_j
                     if $N == 1
-                        @views q′[slc_out] = $op(temp[1:r_h], m, q[slc_in])
+                        @views q′[slc_out] = $op!(temp[1:r_h], m, q[slc_in])
                     else
-                        @views q′[slc_out, :] = $op(temp[1:r_h, :], m, q[slc_in, :])
+                        @views q′[slc_out, :] = $op!(temp[1:r_h, :], m, q[slc_in, :])
                     end
                 end
 
@@ -62,57 +64,59 @@ for N in (1, 2)
 
             return q′
         end
-    end
 
-    @eval function kron_mv_fast_square!(out::AbstractArray{T, $N}, x::AbstractArray{T, $N}, matrices::NTuple{M, AbstractMatrix{T}}) where {T, M}
-        ns::Vector{Int} = [size(m, 1) for m in matrices]
-        i_left::Int = 1
-        i_right::Int = prod(ns)
+        square_func! = Symbol("kron_", op, "_fast_square!")
+        @eval function $square_func!(out::AbstractArray{T, $N}, x::AbstractArray{T, $N}, matrices::NTuple{M, AbstractMatrix{T}}) where {T, M}
+            ns::Vector{Int} = [size(m, 1) for m in matrices]
+            i_left::Int = 1
+            i_right::Int = prod(ns)
 
-        out = copy!(out, x)
-        temp = _alloc_temp_array(maximum(ns), x)
+            out = copy!(out, x)
+            temp = _alloc_temp_array(maximum(ns), x)
 
-        for s in 1:length(ns)
-            n = ns[s]
-            i_right ÷= n
-            _kron_mv_kernel_square!(temp, out, n, i_left, matrices[s], i_right)
-            i_left *= n
-        end
-        return out
-    end
-
-    @eval function kron_mv_fast_rect!(out::AbstractArray{T, $N}, x::AbstractArray{T, $N}, matrices::NTuple{M, AbstractMatrix{T}}) where {T, M}
-        r::Vector{Int} = [size(m, 1) for m in matrices]
-        c::Vector{Int} = [size(m, 2) for m in matrices]
-        out = copy!(out, x)
-
-        i_left::Int = 1
-        i_right::Int = prod(c)
-        stemp = _alloc_temp_array(maximum(r), x)
-
-        for h in 1:length(matrices)
-            r_h, c_h = r[h], c[h]
-            i_right ÷= c_h
-            if r_h == c_h
-                _kron_mv_kernel_square!(stemp, out, r_h, i_left, matrices[h], i_right)
-            else
-                out = _kron_mv_kernel_rect(stemp, out, r_h, c_h, i_left, matrices[h], i_right)
+            for s in 1:length(ns)
+                n = ns[s]
+                i_right ÷= n
+                $square_kernel!(temp, out, n, i_left, matrices[s], i_right)
+                i_left *= n
             end
-            i_left *= r_h
+            return out
         end
 
-        return out
+        rect_func! = Symbol("kron_", op, "_fast_rect!")
+        @eval function $rect_func!(out::AbstractArray{T, $N}, x::AbstractArray{T, $N}, matrices::NTuple{M, AbstractMatrix{T}}) where {T, M}
+            r::Vector{Int} = [size(m, 1) for m in matrices]
+            c::Vector{Int} = [size(m, 2) for m in matrices]
+            i_left::Int = 1
+            i_right::Int = prod(c)
+
+            out = copy!(out, x)
+            temp = _alloc_temp_array(maximum(r), x)
+
+            for h in 1:length(matrices)
+                r_h, c_h = r[h], c[h]
+                i_right ÷= c_h
+                if r_h == c_h
+                    $square_kernel!(temp, out, r_h, i_left, matrices[h], i_right)
+                else
+                    out = $rect_kernel!(temp, out, r_h, c_h, i_left, matrices[h], i_right)
+                end
+                i_left *= r_h
+            end
+
+            return out
+        end
     end
 
-
-    @eval function kronsum_mv_fast!(out::AbstractArray{T, $N}, x::AbstractArray{T, $N}, matrices::NTuple{M, AbstractMatrix{T}}) where {T, M}
+    @eval function kronsum_mul_fast!(out::AbstractArray{T, $N}, x::AbstractArray{T, $N}, matrices::NTuple{M, AbstractMatrix{T}}) where {T, M}
         ns::Vector{Int} = [size(m, 1) for m in matrices]
         i_left::Int = 1
         i_right::Int = prod(ns)
 
         out = fill!(out, zero(T))
         temp = copy(x)
-        stemp = _alloc_temp_array(maximum(ns), x)
+        # should use similar instead, but there seems to be a bug when using copy! with empty SparseArrays
+        small_temp = _alloc_temp_array(maximum(ns), x)
 
         # this loop is technically parallelizable,
         #  though that'd end up using more memory
@@ -120,7 +124,7 @@ for N in (1, 2)
             n = ns[s]
             i_right ÷= n
             copy!(temp, x)
-            out += _kron_mv_kernel_square!(stemp, temp, n, i_left, matrices[s], i_right)
+            out += _kron_mul_kernel_square!(small_temp, temp, n, i_left, matrices[s], i_right)
             i_left *= n
         end
         return out
@@ -128,6 +132,6 @@ for N in (1, 2)
 
     @eval function mul!(out::AbstractArray{T, $N}, K::AbstractKroneckerSum, x::AbstractArray{T, $N}) where T
         matrices = getallsummands(K)
-        return kronsum_mv_fast!(out, x, matrices)
+        return kronsum_mul_fast!(out, x, matrices)
     end
 end

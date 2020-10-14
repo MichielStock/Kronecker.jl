da17cbc21d7efece39d12e74a365d82c222c63c3
Ejaaz Merali <emerali@users.noreply.github.com>
2020-10-14 14:05:46 -0400

    Fix errors in benchmark script

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -3,10 +3,10 @@ using BenchmarkTools
 
 SUITE = BenchmarkGroup()
 
-SUITE["mul_vec"] = BenchmarkGroup("vector", "multiplication")
-SUITE["mul_mat"] = BenchmarkGroup("matrix", "multiplication")
-SUITE["ldiv_vec"] = BenchmarkGroup("vector", "solve")
-SUITE["ldiv_mat"] = BenchmarkGroup("matrix", "solve")
+SUITE["mul_vec"] = BenchmarkGroup(["vector", "multiplication"])
+SUITE["mul_mat"] = BenchmarkGroup(["matrix", "multiplication"])
+SUITE["ldiv_vec"] = BenchmarkGroup(["vector", "solve"])
+SUITE["ldiv_mat"] = BenchmarkGroup(["matrix", "solve"])
 
 ###############################################################################
 
@@ -17,15 +17,14 @@ end
 KProd = kronecker([randn(2,2) for i in 1:10]...)
 KPow = kronecker(randn(2,2), 10)
 KSum = kroneckersum([randn(2,2) for i in 1:10]...)
-v = randn(2^10)
-V = randn(2^10, 7)
+x = randn(2^10)
 
-SUITE["mul_vec"]["all_square"]["KroneckerProduct"] = @benchmarkable ($KProd * $v)
-SUITE["mul_vec"]["all_square"]["KroneckerPower"] = @benchmarkable ($KPow * $v)
-SUITE["mul_vec"]["all_square"]["KroneckerSum"] = @benchmarkable ($KSum * $v)
-SUITE["ldiv_vec"]["all_square"]["KroneckerProduct"] = @benchmarkable ($KProd \ $v)
-SUITE["ldiv_vec"]["all_square"]["KroneckerPower"] = @benchmarkable ($KPow \ $v)
-SUITE["ldiv_vec"]["all_square"]["KroneckerSum"] = @benchmarkable ($KSum \ $v)
+SUITE["mul_vec"]["all_square"]["KroneckerProduct"] = @benchmarkable ($KProd * $x)
+SUITE["mul_vec"]["all_square"]["KroneckerPower"] = @benchmarkable ($KPow * $x)
+SUITE["mul_vec"]["all_square"]["KroneckerSum"] = @benchmarkable ($KSum * $x)
+SUITE["ldiv_vec"]["all_square"]["KroneckerProduct"] = @benchmarkable ($KProd \ $x)
+SUITE["ldiv_vec"]["all_square"]["KroneckerPower"] = @benchmarkable ($KPow \ $x)
+SUITE["ldiv_vec"]["all_square"]["KroneckerSum"] = @benchmarkable ($KSum \ $x)
 
 ###############################################################################
 
@@ -35,11 +34,12 @@ end
 
 KProd = kronecker([randn(3,2) for i in 1:10]...)
 KPow = kronecker(randn(3,2), 10)
+y = randn(3^10)
 
-SUITE["mul_vec"]["all_rectangular"]["KroneckerProduct"] = @benchmarkable ($KProd * $v)
-SUITE["mul_vec"]["all_rectangular"]["KroneckerPower"] = @benchmarkable ($KPow * $v)
-SUITE["ldiv_vec"]["all_rectangular"]["KroneckerProduct"] = @benchmarkable ($KProd \ $v)
-SUITE["ldiv_vec"]["all_rectangular"]["KroneckerPower"] = @benchmarkable ($KPow \ $v)
+SUITE["mul_vec"]["all_rectangular"]["KroneckerProduct"] = @benchmarkable ($KProd * $x)
+SUITE["mul_vec"]["all_rectangular"]["KroneckerPower"] = @benchmarkable ($KPow * $x)
+SUITE["ldiv_vec"]["all_rectangular"]["KroneckerProduct"] = @benchmarkable ($KProd \ $y)
+SUITE["ldiv_vec"]["all_rectangular"]["KroneckerPower"] = @benchmarkable ($KPow \ $y)
 
 ###############################################################################
 
@@ -48,7 +48,8 @@ for k in keys(SUITE)
 end
 
 KP = kronecker([randn(2,2) for i in 1:5]..., [randn(3,2) for i in 1:5]...)
-v = randn(2^10)
+x = randn(2^10)
+y = randn((2^5)*(3^5))
 
-SUITE["mul_vec"]["mixed_squareness"]["KroneckerProduct"] = @benchmarkable ($KP * $v)
-SUITE["ldiv_vec"]["mixed_squareness"]["KroneckerProduct"] = @benchmarkable ($KP \ $v)
+SUITE["mul_vec"]["mixed_squareness"]["KroneckerProduct"] = @benchmarkable ($KP * $x)
+SUITE["ldiv_vec"]["mixed_squareness"]["KroneckerProduct"] = @benchmarkable ($KP \ $y)
